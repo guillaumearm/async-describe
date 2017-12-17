e4583d2062e86913a8d1ba41fb456526c418f7cd
Guillaume ARM <trapcodien@gmail.com>
2017-12-17 14:08:04 +0100

    Implement describe and test methods

--- src/index.js
+++ src/index.js
@@ -1,4 +1,64 @@
-// const describe = (desc, fn) => {
-//
-// };
-module.exports = describe;
+const stdMocks = require('std-mocks');
+const term = require( 'terminal-kit' ).terminal ;
+
+const identity = x => x;
+
+const OK = '✓';
+const KO = '✕';
+
+let error;
+let errorPrinted = false;
+let describeLevel = 0;
+
+/* ************************************************************************** */
+/* describe() */
+/* ************************************************************************** */
+const describe = async (text, fn = identity) => {
+  if (error) return;
+  term.bold(`${'  '.repeat(describeLevel)}${text}\n`);
+  describeLevel++;
+  try {
+    await fn();
+  } catch (e) {
+    if (!errorPrinted) {
+      error = e;
+      term.red(`${e.stack}\n`)
+    }
+  }
+  describeLevel--;
+};
+
+/* ************************************************************************** */
+/* test() */
+/* ************************************************************************** */
+let testRunning = false;
+const test = async (text, fn = identity) => {
+  if (error) return;
+  if (testRunning) {
+    throw new Error('tests cannot be nested')
+  }
+  term(`${'  '.repeat(describeLevel)} ${text}`);
+  testRunning = true;
+  stdMocks.use();
+  try {
+    await fn();
+  } catch (e) {
+    error = e
+  }
+  stdMocks.restore();
+  testRunning = false
+  if (error) term.red(` ${KO}`)
+  else term.green(` ${OK}`);
+  term('\n')
+  const output = stdMocks.flush()
+  output.stderr.forEach(x => term.red(x))
+  output.stdout.forEach(x => term.yellow(x))
+  if (error && !errorPrinted) {
+    term.red(`${error.stack}\n`)
+    errorPrinted = true;
+    throw error;
+  }
+}
+
+/* ************************************************************************** */
+module.exports = { describe, test }
